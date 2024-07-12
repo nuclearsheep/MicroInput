1cadfb661db46a7aaffe15b0479cc653f7a711b4
nuclearsheep <nuclearsheep53@Gmail.com>
2024-07-11 20:03:43 -0700

    Update code.py
    Migrated code between lettersc.mpy to work with scanning and the new binary morse code dictionary format

--- CIRCUITPY-2/code.py
+++ CIRCUITPY-2/code.py
@@ -1,6 +1,7 @@
 import lettersc
 from digitalio import DigitalInOut, Direction, Pull
 import board
+from time import sleep
 A = DigitalInOut(board.D3)
 B = DigitalInOut(board.D4)
 C = DigitalInOut(board.D5)
@@ -16,28 +17,71 @@ import disposable
 rows = disposable.rows(row_pins)
 cols = disposable.cols(col_pins)
 del disposable
-prevstate = ONE.value
+prevstate = False
+signal = 0
+silence = 0
+dits = 0
+bits = 0
+UNIT_TIME = 100
 
 while True:
-    for i in row_pins:
-        i.direction = Direction.INPUT
-        i.pull = Pull.UP
-    for i in col_pins:
-        i.direction = Direction.INPUT
-        i.pull = Pull.UP
-    for row in rows:
-        row_pins[row].direction = Direction.OUTPUT
-        row_pins[row].value = False
-        for col in cols:
-            if not col_pins[col].value:
-                coords = (row, col)
-                print(coords)
-                lettersc.lookup(coords, ONE)
-            else:
-                pass
-        lettersc.Release(ONE)
-        row_pins[row].direction = Direction.INPUT
-        row_pins[row].pull = Pull.UP
 
+	for i in row_pins:
+		if i == ONE:
+			pass
+		else:
+			i.direction = Direction.INPUT
+			i.pull = Pull.UP
+	for i in col_pins:
+		i.direction = Direction.INPUT
+		i.pull = Pull.UP
+	for row in rows:
+		row_pins[row].direction = Direction.OUTPUT
+		row_pins[row].value = False
+		for col in cols:
+			if not col_pins[col].value:
+				coords = (row, col)
+				lettersc.lookup(coords, ONE)
+				#lettersc.Release(ONE)
+				while (col == 0) & (~ONE.value):
+					signal += 1
+					sleep(0.001)
+				#else:
+					#signal = 0
+			else:
+				if (prevstate != ONE.value):
+					if not signal:
+						pass
+					elif signal < (UNIT_TIME*2):  # Tally signal to binary
+						dits <<= 1  # Dit
+						bits += 1
+						print("dit")
+					else:
+						dits <<= 1  # Dah
+						dits += 1
+						bits += 1
+						print("dah")
+					silence = 0
+					mask = 0b000011111111
+					dits = ((dits & mask) | (bits << 8))
+					prevstate = ONE.value
+				else:
+					silence += 1
+					if (silence < UNIT_TIME*2):
+						pass
+					elif (silence < (UNIT_TIME*6)):
+						if dits != 0:
+							dits = lettersc.emitLetter(dits)
+							#dits = 0
+							bits = 0
+							signal = 0
+					else:
+						#dits = lettersc.emitLetter(dits)
+						dits = 0
+						bits = 0
+			sleep(0.001)
 
+		#row_pins[row].direction = Direction.INPUT
+		#row_pins[row].pull = Pull.UP
+	#print(bin(dits), signal)
 
